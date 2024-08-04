c1006cd13624c4c27a2e0ee37593988adfa2033b
Jason <sanchezjason@me.com>
2024-08-04 13:29:05 -0600

    swiftui/mapkit_annotations/restroom_finder- MKCoordinateRegion

---
+++ WCFinder/WCFinder/Extensions/MKCoordinateRegion+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  MKCoordinateRegion+Ext.swift
+//  WCFinder
+//
+//  Created by Jason Sanchez on 8/4/24.
+//
+
+import Foundation
+import MapKit
+
+extension MKCoordinateRegion: Equatable {
+    
+    public static func == (lhs: MKCoordinateRegion, rhs: MKCoordinateRegion) -> Bool {
+        if lhs.center.latitude == rhs.center.latitude && lhs.span.latitudeDelta == rhs.span.latitudeDelta && lhs.span.longitudeDelta == rhs.span.longitudeDelta {
+            return true
+        } else {
+            return false
+        }
+    }
+}
