b1373116976cd382a28d91ce4f562ecbd3aac762
Jason <sanchezjason@me.com>
2024-08-04 13:24:48 -0600

    swiftui/mapkit_annotations/restroom_finder- ActionButtons, LocationManager

---
+++ WCFinder/WCFinder/Views/ActionButtons.swift
@@ -0,0 +1,33 @@
+//
+//  ActionButtons.swift
+//  WCFinder
+//
+//  Created by Jason Sanchez on 8/4/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct ActionButtons: View {
+    let mapItem: MKMapItem
+    
+    var body: some View {
+        HStack {
+            
+            Button(action: {
+                MKMapItem.openMaps(with: [mapItem])
+            }, label: {
+                HStack {
+                    HStack {
+                        Image(systemName: "car.circle.fill")
+                        Text("Take me there")
+                    }
+                }
+            }).buttonStyle(.bordered)
+                .tint(.green)
+            
+            Spacer()
+            
+        }
+    }
+}
---
+++ WCFinder/WCFinder/WCFinderView.swift
@@ -0,0 +1,18 @@
+//
+//  WCFinderView.swift
+//  WCFinder
+//
+//  Created by Jason Sanchez on 8/4/24.
+//
+
+import SwiftUI
+
+struct WCFinderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WCFinderView()
+}
