35ba82a0ae0af23ecff3e9e97ee8160ea8e517fe
Jason <sanchezjason@me.com>
2024-08-04 13:18:04 -0600

    swiftui/mapkit_annotations/restroom_finder- setup

---
+++ WCFinder/WCFinder/WCFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WCFinderApp.swift
+//  WCFinder
+//
+//  Created by Jason Sanchez on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WCFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
